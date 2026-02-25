7d5eee82886c8f0fa90e75cefc79a18a55f4ef26
sreedhar <sreedharroy0@gmailcom>
2026-02-24 23:33:27 -0600

    handle click country visited btn commit

--- src/components/Country/Country.jsx
+++ src/components/Country/Country.jsx
@@ -1,19 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Country.css'
 
 const Country = ({ country }) => {
-//    console.log(country)   
-    const handleVisited=()=>{
-        console.log('onclick now')
+    //    console.log(country)   
+    const [visited, setVisited] = useState(false);
+
+    const handleVisited = () => {
+        //    if(visited){
+        //     setVisited(false)
+        //    }
+        //    else{
+        //     setVisited(true)
+        //    }
+
+        // or 
+
+        // setVisited(visited ? false : true)
+
+        // or 
+
+        setVisited(!visited)
     }
     return (
-        <div className='country'>
+        <div className={`country ${visited && 'country-visited'}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h2>{country.name.common}</h2>
             <p>{country.population.population}</p>
             <p>{country.languages.languages.eng}</p>
             <p>Area : {country.area.area} {country.area.area > 300000 ? "(Big Country)" : "(Small Country) "}</p>
-            <button onClick={handleVisited}>Not Visited</button>
+            <button onClick={handleVisited}>
+                {visited ? 'Visited' : 'Not Visited'}
+            </button>
         </div>
     );
 };
